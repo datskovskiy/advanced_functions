508ec736b5b2d58d4bd25e946b1a710bb576b539
anton.datskovskiy <datskovskiy.anton@gmail.com>
2021-04-11 19:31:15 +0300

    Implement code

--- src/advanced-functions.js
+++ src/advanced-functions.js
@@ -2,33 +2,60 @@
 // ------------------------------------ TASK №1
 //=============================================
 const cache = (func) => {
-    // Do your magic here
+    let map = new Map();
+
+    return function (...args) {
+        let key = hash(args);
+        if (map.has(key))
+            return map.get(key);
+
+        let result = func.apply(this, args);
+        map.set(key, result);
+
+        return result;
+    }
+}
+
+function hash(args) {
+    return args.join(',');
 }
 
 //=============================================
 // ------------------------------------ TASK №2
 //=============================================
 const forwardBackwardSteps = {
-    // Do your magic here
+    step: 0,
+
+    forward() {
+        this.step++;
+        return this;
+    },
+
+    backward() {
+        this.step--;
+        return this;
+    },
+
+    revealStep() {
+        console.log(this.step);
+        return this;
+    }
 };
 
 //=============================================
 // ------------------------------------ TASK №3
 //=============================================
-const applyAll = () => {
-    // Do your magic here
-
+const applyAll = (func, ...args) => {
+    return func.apply(this, args);
 }
-const sum = () => {
-    // Do your magic here
-
+const sum = (...args) => {
+    return args.reduce((result, current) => result + current, 0);
 }
 
-const mul = () => {
-    // Do your magic here
-
+const mul = (...args) => {
+    return args.reduce((result, current) => result * current); 
 }
 
 //=============================================
 
-module.exports = {cache, forwardBackwardSteps, applyAll, sum, mul}
+module.exports = { cache, forwardBackwardSteps, applyAll, sum, mul }
